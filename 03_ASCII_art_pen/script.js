680233b92e393e04a454ae7fa95d7395a58694a2
Yanis <yanis.blanchard@gmail.com>
2025-09-12 12:23:58 +0200

    feat(katas): add kata 3 ASCII art pen

---
+++ 03_ASCII_art_pen/script.js
@@ -0,0 +1,19 @@
+const calendrierDuCrayon = {
+    septembre: 10,
+    octobre: 9,
+    novembre: 8,
+    decembre: 7,
+    janvier: 6,
+    fevrier: 5,
+    mars: 4,
+    avril: 3,
+    mai: 2,
+    juin: 1,
+};
+
+function drawMyPen(mois){
+    
+}
+
+drawMyPen()
+
